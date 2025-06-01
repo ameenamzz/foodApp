1ecaac88f6b65fa483373f191fae8813c764324b
ameeenamzz <shoukethameen@gmail.com>
2025-06-01 19:36:14 +0530

    episode 2

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "h1" }, "this is nested"),
-    React.createElement("h2", { id: "h2" }, "this is neste h2"),
+    React.createElement("h2", { id: "h2" }, "this is nest "),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "im 2nd h1"),
