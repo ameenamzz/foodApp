06921ee5e7f4e8035911e88354b376f8cdf29226
ameeenamzz <shoukethameen@gmail.com>
2025-06-24 16:13:27 +0530

    feat: add Contact component with form and update coverage reports

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,14 @@
 const Contact = () => {
   return (
     <div className="contact-div">
-      <h1>Contact Us</h1>
+      <h1 className="font-bold text-2xl text-center p-10">Contact Us</h1>
+      <div className="ml-10">
+        <form>
+          <input className="border-1 mr-5" type="text" placeholder="Name" />
+          <input className="border-1 mr-5" type="text" placeholder="Message" />
+          <button className="bg-gray-300 px-5 py-1 rounded-lg">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
